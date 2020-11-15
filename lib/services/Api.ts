4932935ee387e3a699e313bb5c5c156fb6ebfa6f
Konstantin Kuznetsov <K.Kuznetcov@gmail.com>
2020-11-15 17:19:39 +0300

    chore: bump

--- lib/services/Api.ts
+++ lib/services/Api.ts
@@ -19,7 +19,7 @@ export class Api extends Service {
     super(scope, {
       cluster,
       healthCheck: '/ping',
-      image: 'ghcr.io/whitewater-guide/backend:0.0.452',
+      image: 'ghcr.io/whitewater-guide/backend:0.0.453',
       name: 'api',
       port: 3333,
       environment: {
