aabde5fbfc7800fe58a819dc470b84f5d14e197d
Konstantin Kuznetsov <K.Kuznetcov@gmail.com>
2021-01-31 20:41:57 +0300

    feat: legacy restore task

---
+++ lib/database/LegacyRestoreTask.ts
@@ -0,0 +1,34 @@
+import * as ecs from '@aws-cdk/aws-ecs';
+import * as logs from '@aws-cdk/aws-logs';
+import * as cdk from '@aws-cdk/core';
+
+export interface PGRestoreTaskProps {
+  password: ecs.Secret;
+  host: string;
+}
+
+export class LegacyRestoreTask extends ecs.FargateTaskDefinition {
+  constructor(scope: cdk.Construct, props: PGRestoreTaskProps) {
+    const { password, host } = props;
+
+    super(scope, 'LegacyRestoreTaskDef', { cpu: 1024, memoryLimitMiB: 2048 });
+
+    this.addContainer('LegacyRestoreContainer', {
+      image: ecs.ContainerImage.fromRegistry(
+        'ghcr.io/whitewater-guide/pg_dump_restore:1.8.6',
+      ),
+      environment: {
+        PGHOST: host,
+        PGUSER: 'postgres',
+      },
+      secrets: {
+        PGPASSWORD: password,
+      },
+      logging: new ecs.AwsLogDriver({
+        streamPrefix: 'LegacyRestore',
+        logRetention: logs.RetentionDays.ONE_DAY,
+      }),
+      entryPoint: ['/app/restore_aws.sh'],
+    });
+  }
+}
