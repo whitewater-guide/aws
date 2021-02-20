7233fc14ce88726829df9ac03a9a03829f2fb4e1
Konstantin Kuznetsov <K.Kuznetcov@gmail.com>
2021-02-20 11:31:44 +0300

    fix: unignore configs

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      modules: true,
+    },
+  },
+  plugins: ['@typescript-eslint', 'import', 'simple-import-sort'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+  ],
+  env: {
+    node: true,
+  },
+  rules: {
+    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+    ],
+    // this rule will complain on things like React.memo and React.PureComponent
+    'import/no-named-as-default-member': 'off',
+    'import/order': 'off',
+    'no-console': ['error', { allow: ['info', 'warn', 'error'] }],
+    'no-empty': ['error', { allowEmptyCatch: true }],
+    'prefer-const': ['error', { destructuring: 'all' }],
+    'simple-import-sort/imports': 'error',
+    'sort-imports': 'off',
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+};
