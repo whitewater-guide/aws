9adc4de019e879415eb202133608d9ce10aad3d2
doomsower <K.Kuznetcov@gmail.com>
2021-05-28 12:55:57 +0300

    chore: bump gorge

--- lib/services/Gorge.ts
+++ lib/services/Gorge.ts
@@ -17,7 +17,7 @@ export class Gorge extends Service {
     super(scope, {
       cluster,
       healthCheck: '/version',
-      image: 'ghcr.io/whitewater-guide/gorge:1.25.0',
+      image: 'ghcr.io/whitewater-guide/gorge:1.26.0',
       name: 'gorge',
       port: Gorge.PORT,
       command: [
