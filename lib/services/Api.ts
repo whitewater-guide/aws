871996c79c827eaef279b3a8d47ae991803ba7dd
doomsower <K.Kuznetcov@gmail.com>
2023-08-13 19:30:53 -0400

    chore: deps and fingerprints

--- lib/services/Api.ts
+++ lib/services/Api.ts
@@ -23,7 +23,7 @@ export class Api extends Service {
       healthCheck: {
         path: '/ping',
       },
-      image: 'ghcr.io/whitewater-guide/backend:0.0.502',
+      image: 'ghcr.io/whitewater-guide/backend:0.0.503',
       name: 'api',
       port: Api.PORT,
       environment: {
