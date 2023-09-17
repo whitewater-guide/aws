e3d2b430baca57106f52c203a10864786984b549
doomsower <K.Kuznetcov@gmail.com>
2023-09-17 09:59:51 -0300

    chore: bump

--- lib/services/Api.ts
+++ lib/services/Api.ts
@@ -23,7 +23,7 @@ export class Api extends Service {
       healthCheck: {
         path: '/ping',
       },
-      image: 'ghcr.io/whitewater-guide/backend:0.0.517',
+      image: 'ghcr.io/whitewater-guide/backend:0.0.518',
       name: 'api',
       port: Api.PORT,
       environment: {
