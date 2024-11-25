2dd05fd65e9c5ad8e0372553f0f0b169cdf8ab26
doomsower <12031673+doomsower@users.noreply.github.com>
2024-11-25 20:00:13 -0300

    fix: bump gorge

--- lib/services/Gorge.ts
+++ lib/services/Gorge.ts
@@ -19,7 +19,7 @@ export class Gorge extends Service {
     super(scope, {
       cluster,
       healthCheck: { path: '/version' },
-      image: 'ghcr.io/whitewater-guide/gorge:3.11.2',
+      image: 'ghcr.io/whitewater-guide/gorge:3.11.3',
       name: 'gorge',
       // memory: 2048,
       // cpu: 512,
